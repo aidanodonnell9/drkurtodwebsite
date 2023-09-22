b3071760e2354dddb0b40f8c43729a81665933e8
Aidan O'Donnell <71330478+aidanodonnell9@users.noreply.github.com>
2023-09-21 20:30:06 -0400

    Added navbar folder

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+import { AiOutlineMenu, AiOutlineClose, AiOutlineSearch, AiOutlineUser, AiOutlinePerson } from 'react-icons/ai'
+import Logo from '../../images/logo.png'
+const Navbar = () => {
+    return (
+        <header className={styles.navbar}>
+            <img src={Logo} alt="/" />
+            <nav>
+                <ul className={styles.menu}>
+                    <li>
+                        <a href="/">Learn more</a>
+                    </li><li>
+                        <a href="/">Log in</a>
+                    </li>
+                    <li>
+                        <a href="/">Sign up</a>
+                    </li>
+                    <li>
+                        <AiOutlineSearch />
+                    </li>
+                    <li>
+                        <AiOutlineUser />
+                    </li>
+                </ul>
+            </nav>
+        </header >
+    )
+}
+
+export default Navbar
